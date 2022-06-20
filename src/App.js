66076c4cb7d0904d8603b2111e883492cffefe1d
Maisha Islam <maisha.b.islam@gmail.com>
2022-06-20 13:58:55 +0100

    updated imports to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Footer from './components/Footer';
 import React from 'react';
 import Navigation from './components/Navigation';
+import VenueContainer from './containers/VenueContainer';
+import BookingContainer from './containers/BookingContainer';
+
 
 function App() {
   return (
