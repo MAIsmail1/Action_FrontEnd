08e6793aa2d0225b8ea9e9aed348c0da953d17eb
Mohamed Ismail <mohamedaismail95@gmail.com>
2022-06-20 15:23:23 +0100

    Merge branch 'main' of github.com:MAIsmail1/Action_FrontEnd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
-import Footer from './containers/Footer';
+import Footer from './components/Footer';
+import AboutUs from './components/AboutUs';
 function App() {
   return (
     <div className="App">
+      
+      <AboutUs />
+      
       <Footer />
+
+      
     </div>
   );
 }
