c799015dbe275570f57f9ba53cae0daa3a1cabf3
Mohamed Ismail <mohamedaismail95@gmail.com>
2022-06-20 10:16:57 +0100

    footer

---
+++ src/containers/Footer.js
@@ -0,0 +1,60 @@
+import React from "react"
+
+const Footer = () => {
+
+
+
+
+    return (
+
+        <footer className="footer">
+            <div className="container">
+                <div className="row">
+                    <div className="footer-col">
+                        <h4>Action</h4>
+                        <ul>
+                            <li><a href=""></a>Social Media Links</li>
+                            <li><a href=""></a>Legal</li>
+                            <li><a href=""></a>Copyright</li>
+                            <li><a href=""></a>Last Updated</li>
+                        </ul>
+
+                    </ div>
+                    <div className="footer-col">
+                        <h4>Venues</h4>
+                        <ul>
+                            <li><a href=""></a>Cineworld</li>
+                            <li><a href=""></a>Vue</li>
+                            <li><a href=""></a>Odeon</li>
+                            <li><a href=""></a>Showcase</li>
+                            <li><a href=""></a>Everyman</li>
+                        </ul>
+
+                    </ div>
+                    <div className="footer-col">
+                        <h4>Get Help</h4>
+                        <ul>
+                            <li><a href="">FAQs</a></li>
+                            <li><a href="">Refunds</a></li>
+                            <li><a href="">Complaints</a></li>
+                        </ul>
+
+                    </ div>
+                    <div className="footer-col">
+                        <h4>Follow us</h4>
+                        <div className="social-links">
+                            <a href=""></a>
+                        </div>
+
+                    </ div>
+
+                    </div>
+
+
+            </div>
+
+        </footer>
+    )
+}
+
+export default Footer;
