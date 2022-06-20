c707ca1ae4463fbfe9ce8912115d0b36e6220cb0
Kieran Sandford <kieran.sandford1@gmail.com>
2022-06-20 14:51:10 +0100

    awaiting images to be added

--- src/App.js
+++ src/App.js
@@ -6,14 +6,22 @@ import Carousel, {CarouselItem} from './components/Carousel';
 
 export default function App() {
   return (
+    <p>
     <div className="App">
       <Carousel>
-        <CarouselItem>Item 1</CarouselItem>
-        <CarouselItem>Item 2</CarouselItem>
-        <CarouselItem>Item 3</CarouselItem>
+        <CarouselItem>
+          Image 3
+        </CarouselItem>
+        <CarouselItem>
+          Image 2
+        </CarouselItem>
+        <CarouselItem>
+          Image 3
+        </CarouselItem>
       </Carousel>
 
     </div>
+    </p>
   );
 }
 
