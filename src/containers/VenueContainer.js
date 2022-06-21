a1e75e7190e4ceda5c913887a60c1d2902802377
nkhan24 <naeem.m.khan18@gmail.com>
2022-06-21 13:05:37 +0300

    Axios method works, added getRequest all movies

--- src/containers/VenueContainer.js
+++ src/containers/VenueContainer.js
@@ -8,9 +8,9 @@ const VenueContainer = () => {
 
     return(
         <>
-            {/* <UpdateVenue/>
-            <NewVenue/>
-            {/* <MovieList/> */}
+             {/* <UpdateVenue/>
+            <NewVenue/> */}
+             <MovieList/>
         </>
     )
 }
