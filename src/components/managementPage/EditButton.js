b4f05342b230a4d0f9b7c65c71335b15e93554d0
Maisha Islam <maisha.b.islam@gmail.com>
2022-06-23 12:54:37 +0100

    venue buttons

--- src/components/managementPage/EditButton.js
+++ src/components/managementPage/EditButton.js
@@ -1,20 +1,50 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 
 const EditButton = () => {
+    const [venues, setVenues] = useState([]);
+
+useEffect(() => {
+  axios.get('http://127.0.0.1:8080/venue')
+    .then(res => {
+      const venues = res.data;
+      setVenues(venues);
+    }).catch((err) => console.log(err));
+  }, []
+  );
 
     const navigate = useNavigate();
     return (
         
-            
-            <nav>
-                <ul className="venue-button">
+                <section className="editmovie">
+                <h3>Edit Venues</h3>
+                    <table className="add_film_table">
+                    <td>View All Venues:</td>
+                    <td>
+                  <select name="venue" id="venue">
+                                {
+                    venues.map(venue =>
+                        <option key={venue.id}>{`${venue.id} | ${venue.name}`} 
+                        </option>
+                        )
+                    }
+                  </select>
+                            </td>
+                    </table>
+                   <ul className="venue_buttons">
                     
-                    <a href="" onClick={() => { navigate('list-of-venue') }} ><button>Venue List</button></a>
+                    <a href="" onClick={() => { navigate('list-of-venue') }} ><button>Venue Timetable</button></a>
+
                     <a href="" onClick={() => { navigate('add-venue') }} ><button>Add Venue </button></a>
                     <a href="" onClick={() => { navigate('edit-venue') }} ><button>Edit Venue </button></a>
+                    <a href="" onClick={() => { navigate('edit-venue') }} ><button>Delete Venue </button></a>
+
                 </ul>
-            </nav>
+            
+                </section>
      
     );
            
