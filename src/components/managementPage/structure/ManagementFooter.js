ee20d4dbd0b07373453aeea5f33485db9ed10444
nkhan24 <naeem.m.khan18@gmail.com>
2022-06-23 14:24:42 +0300

    Added Management Footer

---
+++ src/components/managementPage/structure/ManagementFooter.js
@@ -0,0 +1,78 @@
+import React from "react";
+
+
+const ManagementFooter = () => {
+
+    return (
+        <footer className="footer">
+
+    <h3 id="contactlink" className="contactus">Contact Us</h3>
+      <p className="contact">If you have any enquiries or issues, please do not hesitate to drop a message below!</p>
+            <form className="form">
+                <label className="formName">Name:</label>
+                <input 
+                    type="name" 
+                    id="name" 
+                />
+                <label className="formEmail">Email:</label>
+                <input 
+                    type="email" 
+                    id="email" 
+                 />
+                <label className="formEnquiry">Enquiry:</label>
+                <input 
+                    id="enquiry"
+                >
+                    </input> 
+
+                <input type="submit" value="Submit" />
+            </form>
+                <div className="container">
+                    <div className="row">
+                    <div className="footer-col" >
+                        <h4>Action</h4>
+                        <ul>
+
+                            <li><a href="#">Legal</a></li>
+                            <li><a href="#">Copyright</a></li>
+                            <li><a href="#">Last Updated</a></li>
+                        </ul>
+                        </ div>
+
+                    <div className="footer-col">
+                        <h4>Venues</h4>
+                        <ul>
+                            <li><a href="#">Cineworld</a></li>
+                            <li><a href="#">Vue</a></li>
+                            <li><a href="#">Odeon</a></li>
+                            <li><a href="#">Showcase</a></li>
+                            <li><a href="#">Everyman</a></li>
+                        </ul>
+                    </ div>
+
+                    <div className="footer-col">
+                        <h4>Help</h4>
+                        <ul>
+                            <li><a href="#">FAQs</a></li>
+                            <li><a href="#">Refunds</a></li>
+                            <li><a href="#">Complaints</a></li>
+                        </ul>
+                    </div>
+
+                    <div className="footer-col">
+  	 			        <h4>Social media links</h4>
+  	 			        <ul>
+  	 				        <li><a href="#">Facebook</a></li>
+  	 				        <li><a href="#">Twitter</a></li>
+  	 				        <li><a href="#">Instagram</a></li>
+                       </ul>
+  	 			    </div>
+
+                </div>
+            </div>
+        </footer>
+    );
+
+}
+
+export default ManagementFooter;
