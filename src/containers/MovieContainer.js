e1237c946715841938190c220876809661430fc1
Mohamed Ismail <mohamedaismail95@gmail.com>
2022-06-23 13:44:56 +0100

    route changes

---
+++ src/containers/MovieContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MovieList from "../components/customerPage/MovieList";
+const MovieContainer = () => {
+
+
+    return(
+        <>
+            
+            <MovieList/>
+        </>
+    )
+}
+
+export default MovieContainer;
